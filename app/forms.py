8b128f318ffb177bcb6e93e07951bb1a17959840
dcontreras <dcontreras@debian.dcontreras.com>
2020-03-22 11:19:38 -0400

    Agregando campos al formulario con estilos de materialize

--- app/forms.py
+++ app/forms.py
@@ -11,32 +11,32 @@ class BookForm(forms.ModelForm):
     Form that manages the books model fields
     """
     title = forms.CharField(widget=TextInput(attrs={
-        'class':'form-control input-md',
+        'class':'mat-input',
         'style': 'min-width: 0; width: 100%; display: inline;',
     }), required = True)
 
     author = forms.CharField(widget=TextInput(attrs={
-        'class':'form-control input-md',
+        'class':'mat-input',
         'style': 'min-width: 0; width: 100%; display: inline;',
     }), required = True)
 
     publication_date = forms.DateTimeField(widget=TextInput(attrs={
-        'class':'form-control input-md',
+        'class':'mat-input',
         'style': 'min-width: 0; width: 100%; display: inline;',
     }), required = True)
 
     price = forms.FloatField(widget=TextInput(attrs={
-        'class':'form-control input-md',
+        'class':'mat-input',
         'style': 'min-width: 0; width: 100%; display: inline;',
     }), required = True)
 
     pages = forms.IntegerField(widget=TextInput(attrs={
-        'class':'form-control input-md',
+        'class':'mat-input',
         'style': 'min-width: 0; width: 100%; display: inline;',
     }), required = True)
 
     book_type = forms.ChoiceField(label="Book type", widget=Select(attrs={
-            'class':'form-control input-md',
+            'class':'mat-input',
             'style': 'min-width: 0; width: 100%; display: inline;',
         }), choices=book_types)
 
