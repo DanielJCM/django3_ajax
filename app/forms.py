ec719ceae4975094738ad7c407b9ff5f9e6d3fd8
dcontreras <dcontreras@debian.dcontreras.com>
2020-03-21 22:06:39 -0400

    Agregando estilos de materialize

--- app/forms.py
+++ app/forms.py
@@ -2,16 +2,43 @@
 from django import forms
 from app.models import *
 from django.forms import (
-    TextInput, CharField, Select, RadioSelect, Textarea, CheckboxInput
+    IntegerField, DateTimeInput, TextInput, CharField, Select, RadioSelect, Textarea, CheckboxInput, DateTimeField, FloatField
 )
 
 
 class BookForm(forms.ModelForm):
     """
-    Form that manages the person model fields
+    Form that manages the books model fields
     """
+    title = forms.CharField(widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = True)
 
-    publication_date = forms.DateTimeInput()
+    author = forms.CharField(widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = True)
+
+    publication_date = forms.DateTimeField(widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = True)
+
+    price = forms.FloatField(widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = True)
+
+    pages = forms.IntegerField(widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = True)
+
+    book_type = forms.ChoiceField(label="Book type", widget=Select(attrs={
+            'class':'form-control input-md',
+            'style': 'min-width: 0; width: 100%; display: inline;',
+        }), choices=book_types)
 
     class Meta:
 
